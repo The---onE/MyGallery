116a82607b6b7f787fed13387183b40b02515c32
The-_-onE <834489218@qq.com>
2015-11-26 17:11:35 +0800

    PhotoActivity could show the Directory of photos

--- app/src/main/java/com/xmx/mygallery/PhotoActivity.java
+++ app/src/main/java/com/xmx/mygallery/PhotoActivity.java
@@ -7,33 +7,40 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
+import android.widget.TextView;
 
 import com.xmx.mygallery.Adapter.PhotoAdapter;
 import com.xmx.mygallery.Entities.AlbumItem;
 
 public class PhotoActivity extends Activity {
-    private AlbumItem album;
-    private PhotoAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.photo_activity);
 
-        album = (AlbumItem) getIntent().getExtras().get("album");
-
-        GridView gv = (GridView) findViewById(R.id.photo_gridview);
-        adapter = new PhotoAdapter(this, album);
-        gv.setAdapter(adapter);
-        gv.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(PhotoActivity.this, BigPhotoActivity.class);
-                intent.putExtra("paths", album.getPaths());
-                intent.putExtra("index", position);
-                startActivity(intent);
+        final AlbumItem album = (AlbumItem) getIntent().getExtras().get("album");
+        if (album != null) {
+            TextView tv = (TextView) findViewById(R.id.photo_path);
+            String p = album.getPaths().get(0);
+            int end = p.lastIndexOf("/");
+            if (end != -1) {
+                tv.setText(p.substring(0, end));
+            } else {
+                tv.setText(null);
             }
-        });
 
+            GridView gv = (GridView) findViewById(R.id.photo_gridview);
+            PhotoAdapter adapter = new PhotoAdapter(this, album);
+            gv.setAdapter(adapter);
+            gv.setOnItemClickListener(new OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent(PhotoActivity.this, BigPhotoActivity.class);
+                    intent.putExtra("paths", album.getPaths());
+                    intent.putExtra("index", position);
+                    startActivity(intent);
+                }
+            });
+        }
     }
 }
